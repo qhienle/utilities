f0793783da1bb5b9634c02bce1dd4e129178ec1f
Hien Le <qhienle@gmail.com>
2017-02-15 11:42:12 +0100

    Make WordCounter() as a separate Class file

---
+++ wordcounter.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""Count word occurence
+
+USAGE: count_word_occurence.py [--options] input_file
+
+Reports the occurence of each word, as lower-case, in an input file.
+"""
+
+import sys
+
+__version__ = "0.1"
+__author__  = "hien.le@vlimagrain.com"
+
+class WordCounter:
+    def __init__(self, description=None):
+        self.description = "This is the word occurence counter"
+        self.counter = dict()
+
+    def add(self, word=""):
+        lc = word.lower()
+        if lc in self.counter:
+            self.counter[lc] += 1
+        else:
+            self.counter[lc] = 0
+
+    def sort(self):
+        pass
+
+    def lookup(self, word):
+        try:
+            return self.counter[word.lower()]
+        except KeyError:
+            return 0
+
+    def report(self):
+        pass
+
+#=== The real stuff ============================================================
+
+def main():
+    counter = WordCounter()
+    with open (args.infile, "rb")as infile:
+        for line in infile.readlines():
+            words = line.split()
+            for word in words:
+                counter.add(word)
+    competitors = ["bayer", "nunhems", "syngenta", "basf", "dow", "monsanto", "dupont", "pioneer", "seminis", "sakata", "kws", "rijk", "zwaan", "enza", "bejo", "takii", "limagrain"]
+    for competitor in competitors:
+        print( "{}, {}".format(competitor, counter.lookup(competitor)))
+    counter.report()
+
+if __name__ == "__main__":
+    main()
